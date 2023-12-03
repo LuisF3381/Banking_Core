a8f75787271c1a4415104e696ecb338494675325
luisf3382 <a20193232@pucp.edu.pe>
2023-12-03 17:02:45 -0500

    despliegue v1.0

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ app = FastAPI()
 # Configuración de CORS para permitir solicitudes desde http://localhost:3000
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET","POST"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
